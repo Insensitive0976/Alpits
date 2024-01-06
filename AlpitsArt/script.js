6ded9a528b381453882958bea5069840cb4f1578
Insensitive0976 <154105613+Insensitive0976@users.noreply.github.com>
2024-01-06 10:47:21 -0500

    Add files via upload

--- AlpitsArt/script.js
+++ AlpitsArt/script.js
@@ -1,16 +1,34 @@
-let imenu = document.getElementById('iMenu')
+let imenu = document.getElementById('IconMenu')
 let section = document.querySelector('section')
 
 imenu.onclick = function(){
     let widthA = 0
     let widthS = '46vw'
-    let aside3 = document.getElementById('aside3')
-    if(aside3.clientWidth == '0'){
-        widthA=aside3.scrollWidth;
+    let aside2 = document.getElementById('Aside-2')
+    if(aside2.clientWidth == '0'){
+        widthA=aside2.scrollWidth;
     }
     else{
         widthS='71vw'
     }
-    aside3.style.width = widthA+'px';
+    aside2.style.width = widthA+'px';
     section.style.width = widthS;
+}
+
+let mensajescabeza = document.getElementById('Aside-3-Mensajes-Cabeza')
+
+mensajescabeza.onclick = function(){
+    let heightMC = 0
+    let mensajescuerpo = document.getElementById('Aside-3-Mensajes-Cuerpo')
+    let heightA = 0
+    let anuncios = document.getElementById('Aside-3-Anuncios')
+
+    if(mensajescuerpo.clientHeight == '0'){
+        heightMC=mensajescuerpo.scrollHeight
+    }
+    if(anuncios.clientHeight == '0'){
+        heightA = '48vh'
+    }
+    anuncios.style.height = heightA;
+    mensajescuerpo.style.height = heightMC+'px';
 }
