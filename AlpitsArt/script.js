92123cb83771b964f9202208a169b03993022124
Insensitive0976 <154105613+Insensitive0976@users.noreply.github.com>
2024-01-10 20:43:43 -0500

    Add files via upload

--- AlpitsArt/script.js
+++ AlpitsArt/script.js
@@ -1,12 +1,12 @@
+// Menu izquierdo expandible
+
 let IconMenu = document.getElementById('IconMenu');
 
 IconMenu.onclick = function(){
     
     let Aside2 = document.getElementById('Aside-2');
     let Section = document.querySelector('section');
-    let SectionPerfiles = document.getElementById('Section-Perfiles');
-    let SectionArticulos = document.querySelectorAll('article');
-    let SectionFinal = document.getElementById('Section-ArticuloFinal');
+    let SeccionesTamModificable = document.querySelectorAll('.SeccionTamañoModificable');
     let widthAside2 = 0;
     let widthSection = '46vw';
     let fontsize = '0.37vw';
@@ -22,14 +22,13 @@ IconMenu.onclick = function(){
 
     Aside2.style.width = widthAside2+'px';
     Section.style.width = widthSection;
-    SectionPerfiles.style.fontSize = fontsize;
-
-    SectionArticulos.forEach(article => {
-        article.style.fontSize = fontsize;
-    });
-
-    SectionFinal.style.fontSize = fontsize;
+    SeccionesTamModificable.forEach(SeccionTamModificable => {
+        SeccionTamModificable.style.fontSize = fontsize;
+    })
 }
+
+
+// Menu de perfil en menu izquierdo expandible
 
 let A2PerMenEleTitulo = document.querySelectorAll('.Aside-2-Perfil-Menu-Elemento-Titulo');
 
@@ -38,15 +37,15 @@ A2PerMenEleTitulo.forEach(Titulo => {
         
         let ElementoClicado = event.currentTarget;
         let ElementosNoClicados = Array.from(A2PerMenEleTitulo).filter(ElementoNoClicado => ElementoNoClicado !== ElementoClicado);
-
+        
         ElementosNoClicados.forEach(ElementoNoClicado => {
 
             let Espacio = ElementoNoClicado.nextElementSibling;
             let Contenedor = Espacio.firstElementChild;
 
-            Espacio.style.height = '0'
-            Contenedor.style.height = '0'
-            Contenedor.style.width = '0'
+            Espacio.style.height = '0';
+            Contenedor.style.height = '0';
+            Contenedor.style.width = '0';
             ElementoNoClicado.style.backgroundColor = '';
             ElementoNoClicado.style.color = '';
             ElementoNoClicado.style.borderRadius = '0'
@@ -61,9 +60,9 @@ A2PerMenEleTitulo.forEach(Titulo => {
         let width = 0;
         let color = '';
         let bcolor = '';
-        let borderradius = 0
+        let borderradius = 0;
         
-        if(Espacio.clientHeight === 0){
+        if(Espacio.clientHeight == '0'){
             height=Contenido.scrollHeight;
             width=Contenido.scrollWidth;
             color = '#ffffff';
@@ -80,10 +79,155 @@ A2PerMenEleTitulo.forEach(Titulo => {
         Contenedor.style.width = width+'px';
         Titulo.style.color = color;
         Titulo.style.backgroundColor = bcolor;
-        Titulo.style.borderRadius = borderradius
-
+        Titulo.style.borderRadius = borderradius;
+        
     })
 })
+
+
+// Recientes, links
+
+let SelecInicio = document.querySelectorAll('.SelecInicio')
+let SelecPublico = document.querySelectorAll('.SelecPublico')
+
+SelecPublico.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecPublico').click();
+        
+    })
+})
+
+let SelecTodo = document.querySelectorAll('.SelecTodo')
+
+SelecTodo.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecTodo').click();
+        
+    })
+})
+
+let SelecPopular = document.querySelectorAll('.SelecPopular')
+
+SelecPopular.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecPopular').click();
+        
+    })
+})
+
+let SelecArte = document.querySelectorAll('.SelecArte')
+
+SelecArte.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecArte').click();
+        
+    })
+})
+
+let SelecEspecial = document.querySelectorAll('.SelecEspecial')
+
+SelecEspecial.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecEspecial').click();
+        
+    })
+})
+
+let SelecNovedades = document.querySelectorAll('.SelecNovedades')
+
+SelecNovedades.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecNovedades').click();
+        
+    })
+})
+
+let SelecOtros = document.querySelectorAll('.SelecOtros')
+
+SelecOtros.forEach(seleccionador => {
+    seleccionador.addEventListener('click', function() {
+        
+        document.getElementById('SelecOtros').click();
+        
+    })
+})
+
+
+// Informacion extra extraible
+
+let InformacionTitulo = document.getElementById('Aside-2-MasInformacion-Titulo')
+
+InformacionTitulo.onclick = function(){
+
+    InformacionContenedor = document.getElementById('Aside-2-MasInformacion-Contenedor')
+    width = '0';
+    position = 'static';
+
+    if(InformacionContenedor.clientWidth == '0'){
+        width = '46vw'
+        position = 'fixed'
+    }
+
+    // InformacionContenedor.style.position = position
+    InformacionContenedor.style.width = width
+}
+
+// Aviso de seccion
+
+let Xs = document.querySelectorAll('.Section-Seccion-Aviso-X');
+
+Xs.forEach(x => {
+    x.addEventListener('click', ()=>{
+
+        let Avisos = document.querySelectorAll('.Section-Seccion-Aviso');
+        let Informacion = document.querySelectorAll('.Section-Seccion-Aviso-Aviso');
+
+        Informacion.forEach(info => {
+            info.style.display = 'none';
+        })
+        Xs.forEach(x => {
+            x.style.display = 'none';
+        })
+        Avisos.forEach(aviso => {
+            aviso.style.display = 'none';
+        })
+        
+    })
+})
+
+
+// Menu de secciones
+
+const MenuSeccion = document.querySelectorAll('[data-target]')
+const MenuContenido = document.querySelectorAll('[data-content]')
+
+MenuSeccion.forEach(seleccionado => {
+
+    seleccionado.addEventListener('click', () => {
+
+        MenuSeccion.forEach(seleccionador => {
+            seleccionador.classList.remove('SelectorActivo');
+        })
+        MenuContenido.forEach(contenido => {
+            contenido.classList.remove('SeccionActiva');
+        })
+
+        seleccionado.classList.add('SelectorActivo');
+        const t = document.querySelector(seleccionado.dataset.target);
+        t.classList.add('SeccionActiva');
+
+    })
+
+})
+
+
+// Menu de mensajes expandible
 
 let A3MenCabeza = document.getElementById('Aside-3-Mensajes-Cabeza');
 
@@ -105,5 +249,3 @@ A3MenCabeza.onclick = function(){
     Anuncio.style.height = heightAnuncio;
     Cuerpo.style.height = heightCuerpo+'px';
 }
-
-
